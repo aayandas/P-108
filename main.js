7b3d02c749d69fffeda378cebf33a7e98e209a7e
aayandas <94917206+aayandas@users.noreply.github.com>
2022-04-27 18:23:27 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+//https://teachablemachine.withgoogle.com/models/O4Ic8zKk8/
+
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/O4Ic8zKk8/model.json', modelReady);   
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+
+    if (error){
+        console.error(error);
+    } else{
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = 'I can hear - '+results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+"%";
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color ="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+
+        img = document.getElementById('dog1');
+        img1 = document.getElementById('cat1');
+        img2 = document.getElementById('lion1');
+        img3 = document.getElementById('cow1');
+
+        if (results[0].label == "Meow") {
+            img.src = 'cat.gif';
+            img1.src = 'dogg.jpg';
+            img2.src= ' lion.jpg';
+            img3.src= ' cow1.jpg';
+        }
+        else if (results[0].label == ""){
+            img.src = 'cat.jpg';
+            img1.src = 'dogg.gif';
+            img2.src= ' lion.jpg';
+            img3.src= ' cow1.jpg';
+        }
+        else if (results[0].label == "Snapping"){
+            img.src = 'cat.jpg';
+            img1.src = 'dogg.jpg';
+            img2.src= ' lion.gif';
+            img3.src= ' cow1.jpg';
+        }
+        else{
+            img.src = 'cat.jpg';
+            img1.src = 'dogg.jpg';
+            img2.src= ' lion.jpg';
+            img3.src= ' cow1.gif';
+        }
+     
+    }
+        
+}
